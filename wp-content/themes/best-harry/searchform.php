eaf9532aa3405e68e32e467e166984f5e6ef1a61
Harry Dowe <harry.dowe@gmail.com>
2015-07-07 23:01:34 +0100

    Changed placeholder text for search.

--- wp-content/themes/best-harry/searchform.php
+++ wp-content/themes/best-harry/searchform.php
@@ -5,7 +5,7 @@
  * @since best 1.0
  */ ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="search" name="s" id="s" placeholder="<?php _e( 'Enter search keyword', 'best' ); ?>" value="<?php echo esc_attr( the_search_query() ); ?>"/>
+	<input type="text" class="search" name="s" id="s" placeholder="<?php _e( 'Search', 'best' ); ?>" value="<?php echo esc_attr( the_search_query() ); ?>"/>
 	<button type="submit" value=""></button>
 	<div class="best-clear"></div>
 </form><!-- .search-form -->
