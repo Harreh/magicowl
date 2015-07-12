8307e7cfb4478384af525f39774fe8336e4b6b81
Harry Dowe <harry.dowe@gmail.com>
2015-07-12 17:31:47 +0100

    Added social links to footer.

--- wp-content/themes/best-harry/footer.php
+++ wp-content/themes/best-harry/footer.php
@@ -18,7 +18,25 @@
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 					</div> <!-- best-footer-logo-content-home -->
 					<footer class="best-footer-logo-content-by">
-					</footer> <!-- best-footer-logo-content-by -->					
+                        <a href="https://www.facebook.com/magicowldiscos">
+                            <span class="fa-stack fa-2x">
+                              <i class="fa fa-circle fa-stack-2x"></i>
+                              <i class="fa fa-facebook-square fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                        <a href="mailto:tim@magicowldiscos.co.uk">
+                            <span class="fa-stack fa-2x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                        <a id="footer-phone-link">
+                            <span class="fa-stack fa-2x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-mobile fa-lg fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+					</footer> <!-- best-footer-logo-content-by -->
 				</div> <!-- best-footer-logo_content -->
 				<div class="best-clear"></div>
 			</div> <!-- best-footer-logo -->
