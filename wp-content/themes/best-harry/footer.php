8bed5b7ac008236a9fe88b6d1b2c89be4c98b6eb
Harry Dowe <harry.dowe@gmail.com>
2015-07-17 20:08:13 +0100

    Removed stupid comments.

--- wp-content/themes/best-harry/footer.php
+++ wp-content/themes/best-harry/footer.php
@@ -8,15 +8,15 @@
  * @since Best 1.0
  */
 ?>
-			<div class="best-clear"></div><!-- clear -->
-		</div> <!-- home -->	
+			<div class="best-clear"></div>
+		</div>
 		<div id="best_footer"> <?php 
 			get_sidebar( 'footer' ); ?>
 			<div class="best-footer-logo">
 				<div class="best-footer-logo-content">
 					<div class="best-footer-logo-content-home">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-					</div> <!-- best-footer-logo-content-home -->
+					</div>
                     <div class="best-footer-extra">
                         <a href="<?php echo esc_url( home_url( '/privacy-policy' ) ); ?>">Privacy Policy</a>
                     </div>
@@ -39,12 +39,12 @@
                                 <i class="fa fa-mobile fa-lg fa-stack-1x fa-inverse"></i>
                             </span>
                         </a>
-					</footer> <!-- best-footer-logo-content-by -->
-				</div> <!-- best-footer-logo_content -->
+					</footer>
+				</div>
 				<div class="best-clear"></div>
-			</div> <!-- best-footer-logo -->
-		</div> <!-- #best_foote -->
-	</div> <!-- wrapper --> 
+			</div>
+		</div>
+	</div>
 	<?php wp_footer(); ?>
 </body>
 </html>
